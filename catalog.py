ac44c03ade3c4aa80e39d09de80170f6fe709f09
Christian Borresen <christian@Christians-MacBook-Pro.local>
2017-01-26 15:57:52 +0400

    Add full path to fb_client_secrets.json

--- catalog.py
+++ catalog.py
@@ -216,8 +216,8 @@ def fbconnect():
     print "access token received %s" % access_token
 
     # Exchange client token for long-lived server-side token with GET /oauth/access_token?grant_type=fb_exchange_token&client_id={app-id}&client_secret={app-secret}&fb_exchange_token={short-lived-token}
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
-    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    app_id = json.loads(open('/var/www/app/app/fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('/var/www/app/app/fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
